cdd6460719edf91c409fdae98a7dd5b6bf207fb6
=muskan-batra <68785899+muskan-batra@users.noreply.github.com>
2023-05-21 16:28:43 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Header from './componnets/Header';
+import Home from './componnets/Home';
+import './styles/App.scss'
+
+import {Routes , Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     
+      
+       <Header />
+
+         <Routes>
+
+         <Route path="*" element={<Home/>} />
+    <Route path="*" element={<div> Page Not Found </div>} />
+     </Routes> 
+       
+ 
     </div>
   );
 }
---
+++ src/componnets/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link  } from 'react-router-dom'
+import './../styles/Header.scss'
+
+function Header() {
+  return (
+    <nav> 
+        <h1> Star and Star </h1>
+      <main>
+      <Link to = {"."} >Home</Link>
+      <Link to = {"./About"} >About</Link>
+      <Link to = {"./Contact"} >Contact</Link>
+      <Link to = {"./brands"} >Brands</Link>
+      <Link to = {"./Services"} >Servicest</Link>
+      </main>
+    </nav>
+  )
+}
+
+export default Header
